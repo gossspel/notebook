591eb403a0885e05e3338673333bb30dac8d2df1
Sunny Chan <sunnyucb@gmail.com>
2016-07-04 16:57:03 -0700

    Adding NoteBookContent

---
+++ imports/ui/NoteBookContent.jsx
@@ -0,0 +1,11 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class NoteBookContent extends Component {
+    render() {
+        return (
+            <div className="note-book-content">
+                Hello This is NoteBookContent!
+            </div>
+        );
+    }
+}

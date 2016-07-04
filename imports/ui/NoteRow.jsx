b519528be240a1c6b91133c7e35991915b713f80
Sunny Chan <sunnyucb@gmail.com>
2016-07-04 16:55:38 -0700

    Adding classNames to NoteRow title and content.

--- imports/ui/NoteRow.jsx
+++ imports/ui/NoteRow.jsx
@@ -4,8 +4,8 @@ export default class NoteRow extends Component {
     render() {
         return (
             <div className="note-row">
-                <div>{this.props.note.title}</div>
-                <div>{this.props.note.content}</div>
+                <div className="note-row-title">{this.props.note.title}</div>
+                <div className="note-row-content">{this.props.note.content}</div>
             </div>
         );
     }

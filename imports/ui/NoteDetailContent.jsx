8e0ca4976c5bfa7bba51a2d53172b2e150fbe875
Sunny Chan <sunnyucb@gmail.com>
2016-07-05 08:51:44 -0700

    Adding testing implementation of TinyMCE

--- imports/ui/NoteDetailContent.jsx
+++ imports/ui/NoteDetailContent.jsx
@@ -3,7 +3,10 @@ import React, { Component, PropTypes } from 'react';
 export default class NoteDetailContent extends Component {
     render() {
         return (
-            <div className="note-detail-content">This is note content!</div>
+            <form method="post">
+                <div className="note-detail-content" id="note-detail-content-id">Click here to edit!</div>
+                <input type="submit" value="Submit" />
+            </form>
         );
     }
 }

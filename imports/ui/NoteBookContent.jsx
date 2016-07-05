83949840ec4a15708c5e604b33324e7659a2d1fe
Sunny Chan <sunnyucb@gmail.com>
2016-07-04 17:22:25 -0700

    Applying NoteDetailTitle and NoteDetailContent.

--- imports/ui/NoteBookContent.jsx
+++ imports/ui/NoteBookContent.jsx
@@ -1,10 +1,13 @@
 import React, { Component, PropTypes } from 'react';
+import NoteDetailTitle from './NoteDetailTitle';
+import NoteDetailContent from './NoteDetailContent';
 
 export default class NoteBookContent extends Component {
     render() {
         return (
             <div className="note-book-content">
-                Hello This is NoteBookContent!
+                <NoteDetailTitle />
+                <NoteDetailContent />
             </div>
         );
     }

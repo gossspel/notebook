911ff5f143603a9b5d614aa418b64f9da2cf1abe
Sunny Chan <sunnyucb@gmail.com>
2016-07-04 17:21:45 -0700

    Adding NoteDetailTitle.

---
+++ imports/ui/NoteDetailTitle.jsx
@@ -0,0 +1,9 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class NoteDetailTitle extends Component {
+    render() {
+        return (
+            <div className="note-detail-title">This is note title!</div>
+        );
+    }
+}

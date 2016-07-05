b26496b80c415c6ec0145b8bc1ddb258f70a77c9
Sunny Chan <sunnyucb@gmail.com>
2016-07-04 17:22:06 -0700

    Adding NoteDetailContent.

---
+++ imports/ui/NoteDetailContent.jsx
@@ -0,0 +1,9 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class NoteDetailContent extends Component {
+    render() {
+        return (
+            <div className="note-detail-content">This is note content!</div>
+        );
+    }
+}

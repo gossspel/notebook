37919275958aa48e7e5d73ef6f47d9fc3acc0ef0
Sunny Chan <sunnyucb@gmail.com>
2016-07-05 08:51:20 -0700

    Editing some CSS and HTML.

--- imports/ui/NoteBookContent.jsx
+++ imports/ui/NoteBookContent.jsx
@@ -6,6 +6,11 @@ export default class NoteBookContent extends Component {
     render() {
         return (
             <div className="note-book-content">
+                <div className="note-book-top-bar">
+                    <div className="note-book-logo"></div>
+                    <div className="note-book-brand" id="note-book-brand-id">Sunnynote</div>
+                    <div className="note-book-toolbar" id="note-book-toolbar-id"></div>
+                </div>
                 <NoteDetailTitle />
                 <NoteDetailContent />
             </div>
--- imports/ui/NoteDetailTitle.jsx
+++ imports/ui/NoteDetailTitle.jsx
@@ -3,7 +3,7 @@ import React, { Component, PropTypes } from 'react';
 export default class NoteDetailTitle extends Component {
     render() {
         return (
-            <div className="note-detail-title">This is note title!</div>
+            <div className="note-detail-title" id="note-detail-title-id">This is note title!</div>
         );
     }
 }

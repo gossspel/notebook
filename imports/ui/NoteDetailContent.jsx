845cc60771a53fb2470bdecc80f1e6e891171e01
Sunny Chan <sunnyucb@gmail.com>
2016-07-05 22:31:45 -0700

    Removing unused div.

--- imports/ui/NoteDetailContent.jsx
+++ imports/ui/NoteDetailContent.jsx
@@ -3,12 +3,10 @@ import React, { Component, PropTypes } from 'react';
 export default class NoteDetailContent extends Component {
     render() {
         return (
-            <div className="note-detail-tinymce" id="note-detail-tinymce-id">
-                <form method="post">
-                    <div className="note-detail-content" id="note-detail-content-id">Click here to edit!</div>
-                    <input type="submit" value="Submit" />
-                </form>
-            </div>
+            <form method="post">
+                <div className="note-detail-content" id="note-detail-content-id">Click here to edit!</div>
+                <input type="submit" value="Submit" />
+            </form>
         );
     }
 }

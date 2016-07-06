4a52608d1d28e1988e07214fbfe67938ca5ba573
Sunny Chan <sunnyucb@gmail.com>
2016-07-05 22:01:28 -0700

    Making the Evernote like CSS behavior.

--- imports/ui/NoteDetailContent.jsx
+++ imports/ui/NoteDetailContent.jsx
@@ -3,10 +3,12 @@ import React, { Component, PropTypes } from 'react';
 export default class NoteDetailContent extends Component {
     render() {
         return (
-            <form method="post">
-                <div className="note-detail-content" id="note-detail-content-id">Click here to edit!</div>
-                <input type="submit" value="Submit" />
-            </form>
+            <div className="note-detail-tinymce" id="note-detail-tinymce-id">
+                <form method="post">
+                    <div className="note-detail-content" id="note-detail-content-id">Click here to edit!</div>
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
         );
     }
 }

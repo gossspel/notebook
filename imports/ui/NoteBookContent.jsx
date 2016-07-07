0def16200524dd373be4a695cc3d6a2f54261d30
Sunny Chan <sunnyucb@gmail.com>
2016-07-06 22:13:15 -0700

    Adding NoteBookTopBar.

--- imports/ui/NoteBookContent.jsx
+++ imports/ui/NoteBookContent.jsx
@@ -1,16 +1,13 @@
 import React, { Component, PropTypes } from 'react';
 import NoteDetailTitle from './NoteDetailTitle';
 import NoteDetailContent from './NoteDetailContent';
+import NoteBookTopBar from './NoteBookTopBar';
 
 export default class NoteBookContent extends Component {
     render() {
         return (
             <div className="note-book-content">
-                <div className="note-book-top-bar">
-                    <div className="note-book-logo"></div>
-                    <div className="note-book-brand" id="note-book-brand-id">Sunnynote</div>
-                    <div className="note-book-toolbar" id="note-book-toolbar-id"></div>
-                </div>
+                <NoteBookTopBar />
                 <NoteDetailTitle />
                 <NoteDetailContent />
             </div>

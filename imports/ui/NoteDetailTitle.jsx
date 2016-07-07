7de8f91fcc2fb8028164fb9367cb0057dc1d1801
Sunny Chan <sunnyucb@gmail.com>
2016-07-06 22:50:25 -0700

    Making Title editable.

--- imports/ui/NoteDetailTitle.jsx
+++ imports/ui/NoteDetailTitle.jsx
@@ -3,7 +3,16 @@ import React, { Component, PropTypes } from 'react';
 export default class NoteDetailTitle extends Component {
     render() {
         return (
-            <div className="note-detail-title" id="note-detail-title-id">This is note title!</div>
+            <div className="note-detail-title">
+                <form>
+                    <input
+                        className="note-detail-title-input"
+                        type="text"
+                        placeholder="Title your note"
+                        value=""
+                    />
+                </form>
+            </div>
         );
     }
 }

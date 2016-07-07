6b00dea022395587159d55855826f54fc5d4108f
Sunny Chan <sunnyucb@gmail.com>
2016-07-07 07:19:39 -0700

    Form does not need post method.

--- imports/ui/NoteDetailContent.jsx
+++ imports/ui/NoteDetailContent.jsx
@@ -3,7 +3,7 @@ import React, { Component, PropTypes } from 'react';
 export default class NoteDetailContent extends Component {
     render() {
         return (
-            <form method="post">
+            <form>
                 <div className="note-detail-content" id="note-detail-content-id">Click here to edit!</div>
                 <input type="submit" value="Submit" />
             </form>

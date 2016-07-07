a7a54945ab75e6e6591fd4009e2b1a5e6fa6db91
Sunny Chan <sunnyucb@gmail.com>
2016-07-07 09:08:04 -0700

    Putting state in NoteBook.

--- imports/ui/NoteBook.jsx
+++ imports/ui/NoteBook.jsx
@@ -3,11 +3,44 @@ import NoteBookSideBar from './NoteBookSideBar';
 import NoteBookContent from './NoteBookContent';
 
 export default class NoteBook extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            activeTitle: '',
+            activeContent: ''
+        };
+    }
+
+    handleTitleTextInput(titleText) {
+        this.setState({
+            activeTitle: titleText
+        });
+    }
+
+    handleContentTextInput(contentText) {
+        this.setState({
+            activeContent: contentText
+        });
+    }
+
+    handleNoteChange(title, content) {
+        this.setState({
+            activeTitle: title,
+            activeContent: content
+        });
+    }
+
     render() {
         return (
             <div className="note-book">
                 <NoteBookSideBar notes={this.props.notes} />
-                <NoteBookContent />
+                <NoteBookContent
+                    activeTitle={this.state.activeTitle}
+                    activeContent={this.state.activeContent}
+                    onTitleTextInput={this.handleTitleTextInput.bind(this)}
+                    onContentTextInput={this.handleContentTextInput.bind(this)}
+                />
             </div>
         );
     }

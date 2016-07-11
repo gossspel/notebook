e55e6dee0a578b8c28947554f87c436bbd9d4ec7
Sunny Chan <sunnyucb@gmail.com>
2016-07-11 12:40:37 -0700

    Some styling changes.

--- imports/ui/NoteRow.jsx
+++ imports/ui/NoteRow.jsx
@@ -3,9 +3,11 @@ import React, { Component, PropTypes } from 'react';
 export default class NoteRow extends Component {
     render() {
         return (
-            <div className="note-row">
-                <div className="note-row-title">{this.props.note.title}</div>
-                <div className="note-row-content">{this.props.note.content}</div>
+            <div className="note-row-background">
+                <div className="note-row">
+                    <div className="note-row-title">{this.props.note.title}</div>
+                    <div className="note-row-content">{this.props.note.content}</div>
+                </div>
             </div>
         );
     }

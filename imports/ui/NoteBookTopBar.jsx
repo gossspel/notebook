6b64a1e3ac9550e657b8d4921d53ffb7a4949da1
Sunny Chan <sunnyucb@gmail.com>
2016-07-15 08:53:42 -0700

    Making the notebook icon as clickable button.

--- imports/ui/NoteBookTopBar.jsx
+++ imports/ui/NoteBookTopBar.jsx
@@ -4,7 +4,14 @@ export default class NoteBookTopBar extends Component {
     render() {
         return (
             <div className="note-book-top-bar">
-                <div className="note-book-logo"></div>
+                <div className="note-book-logo">
+                    <button className="new-note-button" title="Click me to create new note!" type="button">
+                        <img
+                            className="new-note-button-image"
+                            src="/images/sunnynote.png"
+                        />
+                    </button>
+                </div>
                 <div className="note-book-brand" id="note-book-brand-id">Sunnynote</div>
                 <div className="note-book-toolbar" id="note-book-toolbar-id"></div>
             </div>

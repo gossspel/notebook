51a28c692732c0194ae86e33785a09cf301f34a1
Sunny Chan <sunnyucb@gmail.com>
2016-07-15 00:35:42 -0700

    Stripping html tags and truncating the string in the NoteRow in SideBar.

--- imports/ui/NoteRow.jsx
+++ imports/ui/NoteRow.jsx
@@ -5,12 +5,16 @@ export default class NoteRow extends Component {
         this.props.onNoteChange(this.props.note._id, this.props.note.title, this.props.note.content);
     }
 
+    getCleanedContent() {
+        return this.props.note.content.replace(/(<([^>]+)>|&nbsp;)/ig, "").substring(0, 100);
+    }
+
     render() {
         return (
             <div className="note-row-background" onClick={this.handleClick.bind(this)}>
                 <div className="note-row">
                     <div className="note-row-title">{this.props.note.title}</div>
-                    <div className="note-row-content">{this.props.note.content}</div>
+                    <div className="note-row-content">{this.getCleanedContent()}</div>
                 </div>
             </div>
         );
